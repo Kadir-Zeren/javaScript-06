9190bed4f4de7d13e17a6c06bb928aa5bb477407
kadir-zeren <k4dzeren@gmail.com>
2024-04-29 18:06:16 +0300

    nested function

--- arrow.js
+++ arrow.js
@@ -1,12 +1,48 @@
-const calculateAge = (year) => new Date().getFullYear() - year;
+// const calculateAge = (year) => new Date().getFullYear() - year;
 
-console.log(`AGE: ${calculateAge(1990)}`);
+// console.log(`AGE: ${calculateAge(1990)}`);
 
-const h = Number(prompt("h:"));
-const r = Number(prompt("r:"));
+// const h = Number(prompt("h:"));
+// const r = Number(prompt("r:"));
 
-const volume = (r, h) => Math.PI * r * r * h;
-console.log("VOLUME:", volume(r, h).toFixed(2));
-console.log("VOLUME:", volume(r, h).toPrecision(4));
+// const volume = (r, h) => Math.PI * r * r * h;
+// console.log("VOLUME:", volume(r, h).toFixed(2));
+// console.log("VOLUME:", volume(r, h).toPrecision(4));
 
-console.log("VOLUME:", volume(r, h).toPrecision(5));
+// console.log("VOLUME:", volume(r, h).toExponential(5));
+const add = (n1, n2) => n1 + n2;
+const sub = (n1, n2) => n1 - n2;
+const mul = (n1, n2) => n1 * n2;
+const div = (n1, n2) => n1 / n2;
+
+const compute = (n1, n2, operator) => {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      //   result = n1 + n2;
+      result = add(n1, n2);
+      break;
+    case "-":
+      result = sub(n1, n2);
+      break;
+    case "x":
+    case "*":
+      result = mul(n1, n2);
+      break;
+    case "/":
+      result = div(n1, n2);
+      break;
+
+    default:
+      alert("Incorrect Operator");
+      result = "";
+      break;
+  }
+  return result;
+};
+
+const n1 = +prompt("First Number:");
+const operator = prompt("Operator:");
+const n2 = +prompt("Second Number:");
+
+console.log(`${n1}${operator}${n2}= ${compute(n1, n2, operator)}`);
